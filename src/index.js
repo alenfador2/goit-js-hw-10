703022767211d1baaa6879986590bd351e8eb381
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-12-12 13:43:36 +0100

    Update 1.5

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 const breedSelector = document.querySelector('.breed-select');
+const selectContainer = document.querySelector('.select-container');
 const loader = document.querySelector('.loader');
 const errorInfo = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
@@ -14,7 +16,9 @@ axios.defaults.headers.common['x-api-key'] =
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 errorInfo.style.display = 'none';
+selectContainer.style.display = 'none';
 breedSelector.setAttribute('id', 'single');
+loader.innerText = '';
 function loading() {
   loader.innerText = '';
   loader.style.display = 'block';
@@ -30,13 +34,19 @@ fetchBreeds()
       breed => `<option value = "${breed.id}">${breed.name}</option>`
     );
     breedSelector.innerHTML = html;
-    loader.style.display = 'none';
     closeLoader();
-    // breedSelector = new SlimSelect({ select: '#single' });
+    new SlimSelect({
+      select: '#single',
+      settings: { placeholderText: 'Select your cat' },
+    });
+    selectContainer.style.display = 'block';
   })
   .catch(error => {
     Notiflix.Report.failure(errorInfo.textContent);
     console.error(error);
+  })
+  .finally(() => {
+    console.log('fetchBreeds has ended');
   });
 
 breedSelector.addEventListener('change', ev => {
@@ -45,7 +55,7 @@ breedSelector.addEventListener('change', ev => {
     .then(cats => {
       const catArray = cats.map(cat => {
         console.log(JSON.stringify(cat));
-        return `<img class = "image" src ="${cat.url}" width = "400px" heigth = "400px"></img><div class = "content"><h2>Cat name: ${cat.breeds[0].name}</h2><p class = "text">${cat.breeds[0].description}</p></br><p><b>Temperament: </b>${cat.breeds[0].temperament}</p></div>`;
+        return `<img class = "image" src ="${cat.url}" width = "400px" height = "400px"></img><div class = "content"><h2 class = "header">Cat name: ${cat.breeds[0].name}</h2><p class = "text">${cat.breeds[0].description}</p><p class = "temp"><b>Temperament: </b>${cat.breeds[0].temperament}</p></div>`;
       });
       catInfo.innerHTML = catArray.join('');
     })
